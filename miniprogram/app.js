dbc7ab2aef5f44d90efb863d6b245ddd2ee8d7e9
SLJ2000 <1343239288@qq.com>
2020-06-02 16:31:36 +0800

    实战2

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,11 +1,12 @@
 //app.js
 App({
   onLaunch: function () {
-    
+    // 初始化云开发数据库
     if (!wx.cloud) {
       console.error('请使用 2.2.3 或以上的基础库以使用云能力')
     } else {
       wx.cloud.init({
+        // 云开发设置里的ID
         env:'slj-byugk'
         
       })
--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -4,10 +4,77 @@ const DB = wx.cloud.database().collection("list")
 
 Page({
   data: {
-    
+   
    name:'',
-   age:''
+   age:'',
+   id:''
   },
+  // 更新
+  updhandleage(event){
+    console.log(event),
+
+    this.setData({
+      age:event.detail.value     
+    })
+    
+  },
+  updhandlename(event){
+    console.log(event),
+
+    this.setData({
+      name:event.detail.value     
+    })
+  },
+  updhandleid(event){
+    console.log(event)
+    this.setData({
+      id:event.detail.value
+    })
+  },
+  updata(){
+    DB.doc(this.data.id).update({
+      data:{
+        name:this.data.name,
+         age:this.data.age
+      },
+      success(res){
+        console.log('更新成功',res)
+      },
+      fail(res){
+        console.log('更新失败',res)
+      }
+    })
+  },
+
+  // 删除
+  handleid(event){
+    console.log(event)
+    this.setData({
+      id:event.detail.value
+    })
+  },
+  deldata(){
+    
+    DB.doc(this.data.id).remove({
+      success(res){
+        console.log('删除成功',res)
+      },
+      fail(res){
+        console.log('删除失败',res)
+      }
+
+    })
+  },
+  // 查询
+  handleselect(){
+    DB.get({
+      success(res){
+        console.log('查询数据成功',res)
+       
+      }
+    })
+  },
+  // 增加
   handleage(event){
     console.log(event),
 
@@ -23,6 +90,7 @@ Page({
       name:event.detail.value     
     })
   },
+  
   handlesendinput(){
   
     DB.add({
@@ -38,6 +106,7 @@ Page({
         }
     })
   },
+  // 增加死记录
   handleclick(){
     DB.add({
       data:{
